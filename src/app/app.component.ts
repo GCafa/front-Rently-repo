64c58632548c879ab6bb76b04ec0f1595f053dad
giovannicafarelli <giovannicafarelli03@gmail.com>
2025-06-14 13:11:58 +0200

    Move navbar to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [RouterModule]
+  imports: [RouterModule, NavbarComponent]
 })
 export class AppComponent {
   title = 'ProgettoIngFrontEnd';
