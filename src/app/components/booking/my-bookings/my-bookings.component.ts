1a6d30e4770f2e3920bb18c7ec74b5fe41c2740d
cicciobonfi <francibonfi03@gmail.com>
2025-06-17 20:43:04 +0200

    creazione my-booking, quasi pronto

--- src/app/components/booking/my-bookings/my-bookings.component.ts
+++ src/app/components/booking/my-bookings/my-bookings.component.ts
@@ -3,6 +3,7 @@ import { BookingService } from '../../../services/booking.service';
 import { BookingModel } from '../../../models/booking-model';
 import { UserService } from '../../../services/user.service';
 import { CommonModule, NgIf, NgForOf } from '@angular/common';
+import {ApiPathUtil} from '../../../utils/ApiPathUtil';
 
 @Component({
   selector: 'app-my-bookings',
@@ -19,26 +20,38 @@ export class MyBookingsComponent implements OnInit {
   constructor(
     private bookingService: BookingService,
     private userService: UserService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.userService.getCurrentUser().subscribe({
-      next: (user) => {
-        this.bookingService.getAllUsersBookings(user.id).subscribe({
+      next: (user: { id: any; }) => {
+        if (!user) {
+          this.errorMessage = 'Utente non autenticato.';
+          this.loading = false;
+          return;
+        }
+        const userId = user.id;
+        this.bookingService.getAllUsersBookings(userId).subscribe({
           next: (data) => {
             this.bookings = data;
+            console.log('Prenotazioni recuperate:', this.bookings);
             this.loading = false;
           },
-          error: () => {
-            this.errorMessage = 'Errore nel caricamento delle prenotazioni.';
+          error: (err) => {
+            this.errorMessage = 'Errore durante il caricamento delle Prenotazioni.';
+            console.error(err);
             this.loading = false;
           }
         });
       },
-      error: () => {
+      error: (err) => {
         this.errorMessage = 'Errore nel recupero dell’utente.';
+        console.error(err);
         this.loading = false;
       }
     });
   }
+
+  protected readonly ApiPathUtil = ApiPathUtil;
 }
